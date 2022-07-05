b35f28600942d00e941defff7e8220f2bbdd2119
jpcouhe <98466099+jpcouhe@users.noreply.github.com>
2022-07-05 22:31:53 +0200

    Hover et projets

---
+++ main.js
@@ -0,0 +1,15 @@
+const technologies = document.querySelector(".test");
+
+// document.addEventListener("mousemove", function (e) {
+//     console.log(e);
+// });
+
+// technologies.addEventListener("mouseenter", function (event) {
+//     // on met l'accent sur la cible de mouseenter
+//     event.target.style.color = "purple";
+// });
+
+// technologies.addEventListener("mouseleave", function (event) {
+//     // on met l'accent sur la cible de mouseenter
+//     event.target.style.color = "red";
+// });
