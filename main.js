92e59218f30fcc43ed4e17aa1a20f2c9e19ca09b
jpcouhe <98466099+jpcouhe@users.noreply.github.com>
2022-07-06 11:46:50 +0200

    ScrollTop / Responsive Bar

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
-const technologies = document.querySelector(".test");
+// ------------Menu Responsive--------------
 
-// document.addEventListener("mousemove", function (e) {
-//     console.log(e);
-// });
+const liste = document.querySelector(".liste");
+const presentation = document.querySelector(".presentation");
+const btnResponsive = document.querySelector(".menu_responsive");
 
-// technologies.addEventListener("mouseenter", function (event) {
-//     // on met l'accent sur la cible de mouseenter
-//     event.target.style.color = "purple";
-// });
+btnResponsive.addEventListener("click", () => {
+    liste.classList.toggle("show");
+    console.log(presentation);
+    presentation.classList.toggle("show");
+});
 
-// technologies.addEventListener("mouseleave", function (event) {
-//     // on met l'accent sur la cible de mouseenter
-//     event.target.style.color = "red";
-// });
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 650) {
+        presentation.classList.remove("show");
+        liste.classList.remove("show");
+    }
+});
+
+// -------------------------------------------
+
+// ---------------Scroll Top------------------
+
+const btnScrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", (e) => {
+ 
+    if (window.scrollY < "100") {
+        btnScrollTop.classList.remove("show");
+    } else {
+        btnScrollTop.classList.add("show");
+    }
+});
+
+btnScrollTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
